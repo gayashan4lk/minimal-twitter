b6fe8725ee01a484298bb609edd7f4040c1e84c2
gayashan4lk <gayashan4lk@gmail.com>
2023-11-12 21:23:09 +0530

    Add error handling for post creation and add toast notifications

--- src/components/CreatePostWizard.tsx
+++ src/components/CreatePostWizard.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { api } from "~/utils/api";
 import { type SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 type FormInput = {
   content: string;
@@ -23,7 +24,19 @@ export function CreatePostWizard({ user }: Props) {
         await ctx.posts.getAll.invalidate();
         reset();
       },
-      onError() {
+      onError(error) {
+        switch (error.data?.code) {
+          case "TOO_MANY_REQUESTS":
+            toast.error("Too many requests! Try again later.");
+            break;
+          case "UNAUTHORIZED":
+            toast.error("Unauthorized!");
+            break;
+          default:
+            toast.error("Failed to create post!");
+        }
+
+        console.error(error.message);
         reset();
       },
     });
