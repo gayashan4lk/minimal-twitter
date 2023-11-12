085549764a9cbfd962db4b628f7a3d00216b3f7b
gayashan4lk <gayashan4lk@gmail.com>
2023-11-13 01:15:52 +0530

    Add server-side rendering and prefetching for user profile page

--- src/pages/[profileId].tsx
+++ src/pages/[profileId].tsx
@@ -1,27 +1,64 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
-
+import superjson from "superjson";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
 import { api } from "~/utils/api";
 
-export default function ProfileView() {
-  const router = useRouter();
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson,
+  });
+  const slug = context.params?.profileId as string;
 
-  const { profileId } = router.query;
+  if (!slug) throw new Error("ProfileId not found");
+
+  const profileId = slug.slice(1).toString();
+
+  await helpers.profile.getUserByUserName.prefetch({ userName: profileId });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      profileId,
+    },
+    revalidate: 1,
+  };
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export default function ProfileView(
+  props: InferGetStaticPropsType<typeof getStaticProps>,
+) {
+  const { profileId } = props;
 
   const {
     data: user,
     isLoading,
     isError,
-    error,
   } = api.profile.getUserByUserName.useQuery({
-    userName: profileId!.slice(1).toString(),
+    userName: profileId,
   });
 
   console.log(user);
 
   if (isLoading) return <div>Loading</div>;
 
-  if (isError) return <div>Error occured</div>;
+  if (isError) return <div>Error occurred!</div>;
 
   return (
     <>
